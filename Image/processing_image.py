e255bae6d5de8bbf4cfce14ba43ac19a848c2565
Caleb Duff <cduff4464@yahoo.com>
2016-06-13 16:05:33 -0400

    Removed tabs and replaced with correct spacing

--- Image/processing_image.py
+++ Image/processing_image.py
@@ -4,11 +4,11 @@ from tifffile import imread
 answer = input('Type 1, 2, or 3\n')
 
 if int(answer) == 1:
-	IM = imread('2016_summer_XPD/Image/CdSeNP_000.tif')
+    IM = imread('2016_summer_XPD/Image/CdSeNP_000.tif')
 elif int(answer) == 2:
-	IM = imread('2016_summer_XPD/Image/CdSeNP_001.tif')
+    IM = imread('2016_summer_XPD/Image/CdSeNP_001.tif')
 else:
-	IM = imread('2016_summer_XPD/Image/Ni300K.tif')
+    IM = imread('2016_summer_XPD/Image/Ni300K.tif')
 
 
 imshow(IM)

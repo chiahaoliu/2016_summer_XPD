70814cf2f4cc42c9247beee1e48cbde66a635855
Caleb Duff <cduff4464@yahoo.com>
2016-06-16 10:51:05 -0400

    Finished sliders, will begin switching between files

--- Analyze/analyze2.py
+++ Analyze/analyze2.py
@@ -15,17 +15,17 @@ ce0 = 600
 plt.imshow(pic)
 
 axcolor = 'lightgoldenrodyellow'
-"""
-axrb = plt.axes([0.0,0.2,0.65,0.02], axisbg=axcolor)
-axre = plt.axes([0.0,0.15,0.65,0.02], axisbg=axcolor)
-axcb = plt.axes([0.0,0.1,0.65,0.02], axisbg=axcolor)
-axce = plt.axes([0.0,0.05,0.65,0.02], axisbg=axcolor)
+
+axrb = plt.axes([0.15,0.2,0.5,0.02], axisbg=axcolor)
+axre = plt.axes([0.15,0.15,0.5,0.02], axisbg=axcolor)
+axcb = plt.axes([0.15,0.1,0.5,0.02], axisbg=axcolor)
+axce = plt.axes([0.15,0.05,0.5,0.02], axisbg=axcolor)
 
 rb = Slider(axrb, 'Row Begin', 0, 2047, valinit=rb0)
 re = Slider(axre, 'Row End', 0, 2047, valinit=re0)
 cb = Slider(axcb, 'Col Begin', 0, 2047, valinit=cb0)
 ce = Slider(axce, 'Col End', 0, 2047, valinit=ce0)
-"""
+
 
 avg_ax = plt.axes([0.85,0.2, 0.13, 0.1],axisbg=axcolor)
 std_ax = plt.axes([0.85,0.4, 0.13, 0.1],axisbg=axcolor)
@@ -39,18 +39,18 @@ button4 = Button(max_ax, 'Maximum', color=axcolor, hovercolor='0.975')
 
 def avg(event):
     x = 0
-    if rb0 < re0:
-        rowb = int(rb0)
-        rowe = int(re0)
+    if rb.val < re.val:
+        rowb = int(rb.val)
+        rowe = int(re.val)
     else:
-        rowb = int(re0)
-        rowe = int(rb0)
-    if cb0 < ce0:
-        colb = int(cb0)
-        cole = int(ce0)
+        rowb = int(re.val)
+        rowe = int(rb.val)
+    if cb.val < ce.val:
+        colb = int(cb.val)
+        cole = int(ce.val)
     else:
-        colb = int(ce0)
-        cole = int(cb0)
+        colb = int(ce.val)
+        cole = int(cb.val)
     num = (rowe - rowb)*(cole - colb)
 
     for i in range(rowb, rowe + 1):
@@ -75,18 +75,18 @@ button2.on_clicked(stan_dev)
 
 def get_min(event):
     
-    if rb0 < re0:
-        rowb = int(rb0)
-        rowe = int(re0)
+    if rb.val < re.val:
+        rowb = int(rb.val)
+        rowe = int(re.val)
     else:
-        rowb = int(re0)
-        rowe = int(rb0)
-    if cb0 < ce0:
-        colb = int(cb0)
-        cole = int(ce0)
+        rowb = int(re.val)
+        rowe = int(rb.val)
+    if cb.val < ce.val:
+        colb = int(cb.val)
+        cole = int(ce.val)
     else:
-        colb = int(ce0)
-        cole = int(cb0)
+        colb = int(ce.val)
+        cole = int(cb.val)
 
     z = pic[rowb][colb]
     x = rowb
@@ -102,18 +102,18 @@ button3.on_clicked(get_min)
 
 def get_max(event):
 
-    if rb0 < re0:
-       rowb = int(rb0)
-       rowe = int(re0)
+    if rb.val < re.val:
+       rowb = int(rb.val)
+       rowe = int(re.val)
     else:
-       rowb = int(re0)
-       rowe = int(rb0)
-    if cb0 < ce0:
-       colb = int(cb0)
-       cole = int(ce0)
+       rowb = int(re.val)
+       rowe = int(rb.val)
+    if cb.val < ce.val:
+       colb = int(cb.val)
+       cole = int(ce.val)
     else:
-       colb = int(ce0)
-       cole = int(cb0)
+       colb = int(ce.val)
+       cole = int(cb.val)
 
     z = pic[rowb][colb]
     x = rowb
@@ -127,11 +127,11 @@ def get_max(event):
     print('The maximum is ' + str(z) + ' at ' + str(x) + 'x' + str(y))
 button4.on_clicked(get_max)
 
-"""
-rb.on_changed(print('Moved!'))
-re.on_changed(print('Moved!'))
-cb.on_changed(print('Moved!'))
-ce.on_changed(print('Moved!'))
-"""
+def if_moving(event):
+    pass
+rb.on_changed(if_moving)
+re.on_changed(if_moving)
+cb.on_changed(if_moving)
+ce.on_changed(if_moving)
 
 plt.show()

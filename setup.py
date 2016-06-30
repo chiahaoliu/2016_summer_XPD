81bb7b6fd2ef3064b95dc1b560b865ffe19b9f65
Caleb Duff <cduff4464@yahoo.com>
2016-06-30 13:43:51 -0400

    Added description to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='2016_summer_XPD',
     version='0.0.1',
     packages=find_packages(),
-    description='',
+    description='Experimental code for summer project',
     zip_safe=False,
     url='https://github.com/cduff4464/2016_summer_XPD.git'
 )
